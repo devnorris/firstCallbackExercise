3b156c37add64e6491665a193c990bceab74f30d
Devon Norris <norris.devon@gmail.com>
2018-04-12 17:18:09 +0000

    Finding waldo with forEach method

--- waldo-function.js
+++ waldo-function.js
@@ -1,14 +1,20 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+
+  arr.forEach(function(name, index) {
+    if (name === 'Waldo') {
+      found(index);
     }
-  }
+  })
 }
 
-function actionWhenFound(indexNumber) {
-  console.log("Found him at index " + indexNumber + "!");
+function actionWhenFound(index) {
+  console.log("Found him at index " + index + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+// (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+   // found(i);   // execute callback
